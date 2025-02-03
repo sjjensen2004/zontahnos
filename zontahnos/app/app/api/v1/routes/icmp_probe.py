5a8517efc3dfff88722efeb49b07b4cd7144dcb5
Steve Jensen <sjjensen2004@outlook.com>
2025-02-03 14:37:48 -0700

    Schema fixes

--- zontahnos/app/app/api/v1/routes/icmp_probe.py
+++ zontahnos/app/app/api/v1/routes/icmp_probe.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from app.utils.influx_db_manager import InfluxDBManager
 from app.core.database import get_db
-from app.services.v1 import service_icmp_probe
+from app.services.v1 import service_icmp_probe as service
 from app.schemas.v1 import schema_icmp_probe as schema
 import logging
 
@@ -20,14 +20,14 @@ def create_icmp_probe(body: schema.Create, db: Session = Depends(get_db)):
     Generate a key and database entry to collect ICMP probe metrics.
     """
     # Check if the probe name already exists in PostgreSQL
-    if service_icmp_probe.get_probe_by_name(db, name=body.name):
+    if service.get_probe_by_name(db, name=body.name):
         raise HTTPException(status_code=400, detail="Probe name already exists")
 
     secret_key = secrets.token_hex(16)  # Generate a 32-character hex key
     try:
         # Create the probe in PostgreSQL
         probe_data = schema.ProbeCreate(name=body.name, location=body.location, measurement=body.measurement, secret_key=secret_key)
-        service_icmp_probe.create_probe(db=db, probe_data=probe_data)
+        service.create_probe(db=db, probe_data=probe_data)
         return {"name": body.name, "location": body.location, "measurement": body.measurement, "key": secret_key}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to create probe: {str(e)}")
@@ -39,13 +39,13 @@ def delete_icmp_probe(body: schema.Delete, db: Session = Depends(get_db)):
     Delete an ICMP probe by name.
     """
     # Check if the probe name exists in PostgreSQL
-    db_probe = service_icmp_probe.get_probe_by_name(db, name=body.name)
+    db_probe = service.get_probe_by_name(db, name=body.name)
     if not db_probe:
         raise HTTPException(status_code=404, detail="Probe not found")
 
     try:
         # Delete the probe in PostgreSQL
-        service_icmp_probe.delete_probe(db=db, probe_id=db_probe.id)
+        service.delete_probe(db=db, probe_id=db_probe.id)
         return {"message": f"Probe {body.name} deleted successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to delete probe: {str(e)}")
@@ -58,7 +58,7 @@ def update_icmp_record(body: schema.Update, db: Session = Depends(get_db)):
     """
     try:
         # Check if the secret key is valid in PostgreSQL
-        db_probe = service_icmp_probe.get_probe_by_name(db, name=body.probe_name)
+        db_probe = service.get_probe_by_name(db, name=body.probe_name)
         if not db_probe or db_probe.secret_key != body.key:
             raise HTTPException(
                 status_code=403, detail="Invalid secret key and/or probe name."
@@ -83,7 +83,7 @@ def list_icmp_probes(db: Session = Depends(get_db)):
     List all existing ICMP probes with their name, location and measurement.
     """
     try:
-        probes = service_icmp_probe.list_probes(db)
+        probes = service.list_probes(db)
         return {"probes": probes}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to list probes: {str(e)}")
